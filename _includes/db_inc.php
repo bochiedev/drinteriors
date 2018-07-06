31999ea2b250c461d85ff9b50adf25bee26855f1
bochiedev <bochiegfx@gmail.com>
2018-07-06 11:27:37 +0300

    login sign up admin modals

--- _includes/db_inc.php
+++ _includes/db_inc.php
@@ -3,7 +3,7 @@
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "";
-$db['db_name'] = "drinteriors";
+$db['db_name'] = "cms";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
